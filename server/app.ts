747620f92b1e8de9f6b569a4e1f80c9fcfb3ab85
Nicholas Cheung <ncheung@carboncopies.org>
2024-01-05 15:17:58 -0800

    vryptographic hash

--- server/app.ts
+++ server/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import crypto from "crypto";
 
 const PORT = 8080;
 const app = express();
@@ -8,10 +9,33 @@ const database = { data: "Hello World" };
 app.use(cors());
 app.use(express.json());
 
+const sha256 = (data:string) => {
+  const dataHash= crypto.createHash('sha256');
+  dataHash.update(data);
+  return dataHash.digest('hex');
+}
+
+const hex = sha256(database.data);
+  //cryptographic hash
+
+
 // Routes
 
 app.get("/", (req, res) => {
-  res.json(database);
+  const hex = sha256(database.data);
+  console.log(hex)
+  res.json(hex);
+
+});
+
+app.get("/compromise", (req, res) => {
+  console.log(database);
+  database.data = "compromised";
+  console.log(database);
+
+  const hex = sha256(database.data);
+  console.log(hex)
+  res.json(hex);
 });
 
 app.post("/", (req, res) => {

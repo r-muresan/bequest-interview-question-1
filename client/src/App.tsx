e8a6939419ed4de308e2ad3be14cfb50c56058d5
tanveeshs <Tanveeshs@gmail.com>
2024-01-05 11:57:46 +0530

    Changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,26 @@
 import React, { useEffect, useState } from "react";
-
+import { SHA256, enc } from 'crypto-js';
 const API_URL = "http://localhost:8080";
 
 function App() {
   const [data, setData] = useState<string>();
+  const [hash, setHash] = useState<string>();
+  const [verified, setVerified] = useState<boolean>(true);
 
   useEffect(() => {
     getData();
   }, []);
-
+    const createHash = (data:string|undefined)=> {
+        if(data){
+            return SHA256(data).toString(enc.Hex);
+        }
+        return  "";
+    }
   const getData = async () => {
     const response = await fetch(API_URL);
-    const { data } = await response.json();
+    const { data,hash } = await response.json();
     setData(data);
+    setHash(hash);
   };
 
   const updateData = async () => {
@@ -29,7 +37,13 @@ function App() {
   };
 
   const verifyData = async () => {
-    throw new Error("Not implemented");
+
+      const calcHash = createHash(data);
+      if(calcHash!==hash) {
+          setVerified(false);
+      }else {
+          setVerified(true);
+      }
   };
 
   return (
@@ -62,6 +76,8 @@ function App() {
         <button style={{ fontSize: "20px" }} onClick={verifyData}>
           Verify Data
         </button>
+          {verified && <div style={{color: 'green'}}>Verified</div>}
+            {!verified && <div style={{color: 'red'}}>Not Verified</div>}
       </div>
     </div>
   );
--- server/app.ts
+++ server/app.ts
@@ -1,12 +1,17 @@
 import express from "express";
 import cors from "cors";
-
+import crypto from "crypto";
 const PORT = 8080;
 const app = express();
-const database = { data: "Hello World" };
+const database = { data: "Hello World",hash:createHash("Hello World")};
 
 app.use(cors());
 app.use(express.json());
+function createHash(data: string) {
+  return crypto.createHash("sha256").update(data).digest("hex");
+}
+// The client can recover data by refreshing the data from the server as the server will always contain
+// the latest data. The client can verify the integrity of the data by comparing the hash of the data
 
 // Routes
 
@@ -15,10 +20,14 @@ app.get("/", (req, res) => {
 });
 
 app.post("/", (req, res) => {
-  database.data = req.body.data;
+  const newData = req.body.data;
+  if(newData!=database.data){
+    database.data = newData;
+    console.log("Data changed to: " + newData);
+    database.hash = createHash(newData);
+  }
   res.sendStatus(200);
 });
-
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });

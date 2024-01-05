c74a1697ca04b05e8ed3452e7b0e756a8f634788
George Lugo <machinelearner2334@gmail.com>
2024-01-04 20:03:11 -0800

    Update app.ts

--- server/app.ts
+++ server/app.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import cors from "cors";
+import crypto from "crypto";
 
 const PORT = 8080;
 const app = express();
-const database = { data: "Hello World" };
+const database = { data: "Hello World", hash: "" };
 
 app.use(cors());
 app.use(express.json());
@@ -11,14 +12,27 @@ app.use(express.json());
 // Routes
 
 app.get("/", (req, res) => {
-  res.json(database);
+  res.json(data: database.data, hash: database.hash);
 });
 
 app.post("/", (req, res) => {
-  database.data = req.body.data;
-  res.sendStatus(200);
+  const currentData = req.body.data;
+  const currentHash = dataHash(currentData);
+
+  if (currentHash === req.body.hash) {
+    database.data = currentData;
+    database.hash = currentHash;
+    res.sendStatus(200);
+  } else {
+    res.status(400).send("Integrity check failed")
+  }
 });
 
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
+
+const dataHash = (data) => {
+  const hash = crypto.createHash("sha256").update(data).digest("hex");
+  return hash;
+};

793ae9b4c00a197a2eb0497a1b2077fea422183a
stardev <superman.star25@gmail.com>
2024-01-04 23:07:13 -0500

    update server to handle verfiy and recover

--- server/app.ts
+++ server/app.ts
@@ -1,12 +1,24 @@
 import express from "express";
 import cors from "cors";
+import crypto from "crypto";
 
 const PORT = 8080;
 const app = express();
-const database = { data: "Hello World" };
+
+type DataProps = {
+  data?: string;
+  hash?: string;
+};
+let database: DataProps = { data: "Hello World" };
+let databaseBackup: DataProps = {};
 
 app.use(cors());
 app.use(express.json());
+
+// Hashing function
+function createHash(data: string) {
+  return crypto.createHash("sha256").update(data).digest("hex");
+}
 
 // Routes
 
@@ -15,8 +27,31 @@ app.get("/", (req, res) => {
 });
 
 app.post("/", (req, res) => {
-  database.data = req.body.data;
+  const data = req.body.data;
+
+  // need to backup data before update data.
+  databaseBackup = database;
+
+  database.data = data;
+  database.hash = createHash(data);
+
   res.sendStatus(200);
+});
+
+// Endpoint to verify data
+app.post("/verify", (req, res) => {
+  const currentHash = createHash(req.body.data);
+  if (currentHash === database.hash) {
+    res.json({ isVerified: true });
+  } else {
+    res.json({ isVerified: false });
+  }
+});
+
+// Endpoint to recover data
+app.get("/recover", (req, res) => {
+  database.data = databaseBackup.data;
+  res.json(database);
 });
 
 app.listen(PORT, () => {

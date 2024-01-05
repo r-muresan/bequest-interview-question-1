c9f670790ba5667d7325b84349d6fa0b532318ff
Nolan <nolangelinas@gmail.com>
2024-01-05 18:31:34 -0500

    Add hashing to front end and database

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,70 +1,87 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 const API_URL = "http://localhost:8080";
 
+const generateHash = async (input: string | undefined) => {
+	const encoder = new TextEncoder();
+	const data = encoder.encode(input);
+	const hash = await window.crypto.subtle.digest("SHA-256", data);
+	const hashArray = Array.from(new Uint8Array(hash));
+	const hashHex = hashArray
+		.map((b) => b.toString(16).padStart(2, "0"))
+		.join("");
+	return hashHex;
+};
+
 function App() {
-  const [data, setData] = useState<string>();
+	const [localData, setLocalData] = useState<string>("");
 
-  useEffect(() => {
-    getData();
-  }, []);
+	useEffect(() => {
+		getData();
+	}, []);
 
-  const getData = async () => {
-    const response = await fetch(API_URL);
-    const { data } = await response.json();
-    setData(data);
-  };
+	const getData = async () => {
+		const response = await fetch(API_URL);
+		const { data, hash } = await response.json();
+		localStorage.setItem("hash", hash);
+		setLocalData(data);
+	};
 
-  const updateData = async () => {
-    await fetch(API_URL, {
-      method: "POST",
-      body: JSON.stringify({ data }),
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-    });
+	const updateData = async () => {
+		const newHash = await generateHash(localData);
+		await fetch(API_URL, {
+			method: "POST",
+			body: JSON.stringify({ data: localData, hash: newHash }),
+			headers: {
+				Accept: "application/json",
+				"Content-Type": "application/json",
+			},
+		});
 
-    await getData();
-  };
+		await getData();
+	};
 
-  const verifyData = async () => {
-    throw new Error("Not implemented");
-  };
+	const verifyData = async () => {
+		const response = await fetch(API_URL);
+		const { data, hash } = await response.json();
+		const expectedHash = await generateHash(data);
+		if (hash !== localStorage.getItem("hash") || expectedHash !== hash) {
+			alert("Data is not valid!!");
+		}
+	};
 
-  return (
-    <div
-      style={{
-        width: "100vw",
-        height: "100vh",
-        display: "flex",
-        position: "absolute",
-        padding: 0,
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
-        gap: "20px",
-        fontSize: "30px",
-      }}
-    >
-      <div>Saved Data</div>
-      <input
-        style={{ fontSize: "30px" }}
-        type="text"
-        value={data}
-        onChange={(e) => setData(e.target.value)}
-      />
+	return (
+		<div
+			style={{
+				width: "100%",
+				height: "100%",
+				display: "flex",
+				padding: 0,
+				justifyContent: "center",
+				alignItems: "center",
+				flexDirection: "column",
+				gap: "20px",
+				fontSize: "30px",
+			}}
+		>
+			<div>Saved Data</div>
+			<input
+				style={{ fontSize: "30px" }}
+				type="text"
+				value={localData}
+				onChange={(e) => setLocalData(e.target.value)}
+			/>
 
-      <div style={{ display: "flex", gap: "10px" }}>
-        <button style={{ fontSize: "20px" }} onClick={updateData}>
-          Update Data
-        </button>
-        <button style={{ fontSize: "20px" }} onClick={verifyData}>
-          Verify Data
-        </button>
-      </div>
-    </div>
-  );
+			<div style={{ display: "flex", gap: "10px" }}>
+				<button style={{ fontSize: "20px" }} onClick={updateData}>
+					Update Data
+				</button>
+				<button style={{ fontSize: "20px" }} onClick={verifyData}>
+					Verify Data
+				</button>
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- server/app.ts
+++ server/app.ts
@@ -3,20 +3,21 @@ import cors from "cors";
 
 const PORT = 8080;
 const app = express();
-const database = { data: "Hello World" };
+const database = { data: "Hello World", hash: "123456789" };
 
 app.use(cors());
 app.use(express.json());
 
 // Routes
 
-app.get("/", (req, res) => {
-  res.json(database);
+app.get("/", (_, res) => {
+	res.json(database);
 });
 
 app.post("/", (req, res) => {
-  database.data = req.body.data;
-  res.sendStatus(200);
+	database.data = req.body.data;
+	database.hash = req.body.hash;
+	res.sendStatus(200);
 });
 
 app.listen(PORT, () => {

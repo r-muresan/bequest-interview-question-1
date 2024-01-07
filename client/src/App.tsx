e9618f54121badc03ef08e36531d7c35bcec2281
SamTheBestFishy <samss.lee.21@gmail.com>
2024-01-06 19:04:31 -0800

    setup bcrypt for hash

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,29 +1,77 @@
 import React, { useEffect, useState } from "react";
+import bcrypt from "bcryptjs";
 
 const API_URL = "http://localhost:8080";
 
 function App() {
-  const [data, setData] = useState<string>();
+  const [data, setData] = useState<string>("");
+  const [statusMessage, setStatusMessage] = useState<string | null>(null);
 
-  useEffect(() => {
-    getData();
-  }, []);
+  // useEffect(() => {
+  //   getData();
+  // }, []);
 
   const getData = async () => {
-    const response = await fetch(API_URL);
-    const { data } = await response.json();
-    setData(data);
+
+    try{
+      const response = await fetch(API_URL);
+      const { data: responseData} = await response.json();
+
+      bcrypt.compare(data, responseData, function(err: Error, isMatch: boolean){
+        if(err){
+          throw err;
+        } else if(!isMatch){
+          console.log("data tampered during transit")
+          setStatusMessage("Data tampered during transit. Contact someone for help!");
+        } else {
+          console.log("data successfully stored")
+          setStatusMessage("Data successfully stored");
+          setData(""); // Reset the data in the input textbox
+
+          setTimeout(() => { //erase the message after 3 secconds
+            setStatusMessage(null);
+          }, 3000);
+        }
+
+      })
+    } catch (error) {
+      console.error("ERROR: unsuccessful fetch or parse from server", error);
+    }
+    
   };
 
   const updateData = async () => {
-    await fetch(API_URL, {
-      method: "POST",
-      body: JSON.stringify({ data }),
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-    });
+
+    //chose to hash data so that only the server and the user know the data
+    //data can not even be seen by bequest finance team
+    const salt = bcrypt.genSaltSync(10); //used default 10 saltrounds for now
+    console.log('data',data)
+    const hashedData = bcrypt.hashSync(data, salt)
+
+    console.log(hashedData)
+
+    try {
+      const response = await fetch(API_URL, {
+          method: "POST",
+          body: JSON.stringify({ hashedData }),
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json; charset=utf-8",
+            },
+      });
+
+      if (response.ok) {
+          console.log("API call successful");
+      } else {
+          console.error("API call failed");
+      }
+
+    } catch (error) {
+      console.error("Error during API call:", error);
+    }
+
+    //for sanity check at the moment
+    window.localStorage.setItem('data',hashedData)
 
     await getData();
   };
@@ -56,13 +104,16 @@ function App() {
       />
 
       <div style={{ display: "flex", gap: "10px" }}>
-        <button style={{ fontSize: "20px" }} onClick={updateData}>
+        <button style={{ fontSize: "20px" }} onClick={updateData}> 
           Update Data
         </button>
         <button style={{ fontSize: "20px" }} onClick={verifyData}>
           Verify Data
         </button>
       </div>
+
+      <div style={{ fontSize: "20px", color: "red" }}>{statusMessage}</div>
+
     </div>
   );
 }
--- server/app.ts
+++ server/app.ts
@@ -11,11 +11,15 @@ app.use(express.json());
 // Routes
 
 app.get("/", (req, res) => {
+  console.log("database", database)
   res.json(database);
 });
 
 app.post("/", (req, res) => {
-  database.data = req.body.data;
+  res.header("Content-Type", "application/json; charset=utf-8");
+  res.charset = "utf-8";
+  database.data = req.body.hashedData;
+  console.log("received", database)
   res.sendStatus(200);
 });
 

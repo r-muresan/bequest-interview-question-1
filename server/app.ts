88a7c2f34c7c6af4bf45b46cf55ac7b35a853f08
Nicholas Cheung <ncheung@carboncopies.org>
2024-01-09 12:01:53 -0800

    nodemon

--- server/app.ts
+++ server/app.ts
@@ -9,34 +9,53 @@ const database = { data: "Hello World" };
 app.use(cors());
 app.use(express.json());
 
-const sha256 = (data:string) => {
-  const dataHash= crypto.createHash('sha256');
-  dataHash.update(data);
-  return dataHash.digest('hex');
-}
+const hexHistory: string[] = [];
+  //ordered history of hexes with changes logged
+const history : { [key:string]: any } = {}
+  //use hex as key. Value = the original string data
+//these are ideally stored somewhere else, like another database user profile, and not the server.
 
-const hex = sha256(database.data);
-  //cryptographic hash
+
+// utility functions - 
+const sha256 = async (data:string) => {
+  const dataHash= crypto.createHash('sha256');
+  dataHash.update(data).digest('hex');
+  return dataHash;
+} // makes hash 
+
+
+const storeHistory = async (data: any) => {
+  const hex = await sha256(data.data).toString();
+  if (hexHistory.indexOf(hex) > -1) {
+    
+  } else { //did not have data => stores it
+    hexHistory.push(hex);
+    history[hex]  = {
+      createdOn: new Date(),
+      data
+    };
+  }
+  //how do i make it so that changes to db ALWAYS mean new hex?
+  //otherwise only legit ones have hexes and aren't recorded
+    //and it would be easy to fake a hex anyway...
+} 
 
 
 // Routes
 
 app.get("/", (req, res) => {
-  const hex = sha256(database.data);
-  console.log(hex)
-  res.json(hex);
+  
+  const getHex = sha256(database.data);
+  // console.log(hex)
+  console.log(getHex)
+  // res.json(hex);
 
 });
 
-app.get("/compromise", (req, res) => {
-  console.log(database);
-  database.data = "compromised";
-  console.log(database);
 
-  const hex = sha256(database.data);
-  console.log(hex)
-  res.json(hex);
-});
+app.patch("/", (req, res) => {
+  database.data = "compromise";
+})
 
 app.post("/", (req, res) => {
   database.data = req.body.data;

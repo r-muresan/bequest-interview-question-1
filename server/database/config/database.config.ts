0a009725ddb0618ec113f54f7aa28f5ac8affc8f
Pedro José NPS <pedrojosepaiva@outlook.com>
2024-01-21 13:54:38 -0300

    feat: define charset from database

--- server/database/config/database.config.ts
+++ server/database/config/database.config.ts
@@ -12,6 +12,13 @@ export interface Config {
     connectionTimeout: number;
     requestTimeout: number;
     dialect: string;
+    define: {
+      charset: string;
+      collate: string;
+    }
+    dialectOptions: {
+      charset: string;
+    }
   };
 }
 
@@ -22,12 +29,24 @@ export default function createConfig(env: string): Config {
     database: getEnvVar('DB_NAME') ?? '',
     host: getEnvVar('DB_HOSTNAME') ?? '',
     dialect: 'mysql',
-    logging: console.log, 
+    logging: (sql: string, timing?: number) => {
+      console.log(`SQL Query: ${sql}`);
+      if (timing) {
+        console.log(`Execution Time: ${timing}ms`);
+      }
+    }, 
     options: {
       host: getEnvVar('DB_HOSTNAME') ?? '',
       dialect: 'mysql',
       connectionTimeout: parseInt(getEnvVar('TIMEOUT') || '15000'),
       requestTimeout: parseInt(getEnvVar('TIMEOUT') || '15000'),
+      define: {
+        charset: 'utf8',
+        collate: 'utf8_general_ci',
+      },
+      dialectOptions: {
+        charset: 'utf8mb4', // or the correct charset for your database
+      },
   }
   };
 }

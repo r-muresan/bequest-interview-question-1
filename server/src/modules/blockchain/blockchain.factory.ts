845ccc7ce76227f6076533c6690839d12f02383f
Pedro José NPS <pedrojosepaiva@outlook.com>
2024-01-21 17:31:51 -0300

    fix: removing not used imports

--- server/src/modules/blockchain/blockchain.factory.ts
+++ server/src/modules/blockchain/blockchain.factory.ts
@@ -1,9 +1,6 @@
-import db from "../../../database/database"
 import { Blockchain } from "./blockchain";
 import { BlockchainController } from "./controllers/blockchain.controller";
 import { blockService } from "../block/block.factory";
-import { BlockService } from "../block/services/block.service";
-import { BlockRepository } from "../block/repositories/block.repository";
 
 function BlockchainFactory() {
   const blockchain = Blockchain.getInstance(blockService);

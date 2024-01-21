af435e2ef77c49519d82281bc8b58a59f41142db
Pedro José NPS <pedrojosepaiva@outlook.com>
2024-01-21 10:49:11 -0300

    fix: return await over return when returning a promise

--- server/src/modules/block/services/block.service.ts
+++ server/src/modules/block/services/block.service.ts
@@ -7,22 +7,22 @@ export class BlockService implements IBlockService {
   constructor(private readonly blockRepository: BlockRepository) {}
 
   async getBlockById(id: number): Promise<Block | null> {
-    return this.blockRepository.getBlockById(id);
+    return await this.blockRepository.getBlockById(id);
   }
 
   async getBlockWhere(where: WhereOptions<InferAttributes<Block, { omit: never }>>): Promise<Block | null> {
-    return this.blockRepository.getBlockWhere(where);
+    return await this.blockRepository.getBlockWhere(where);
   }
 
   async addBlock(block: Partial<Block>): Promise<Block> {
-    return this.blockRepository.addBlock(block);
+    return await this.blockRepository.addBlock(block);
   }
 
   async getBlocks(): Promise<Block[]> {
-    return this.blockRepository.getBlocks();
+    return await this.blockRepository.getBlocks();
   }
 
   async countBlocks(): Promise<number> {
-    return this.blockRepository.countBlocks();
+    return await this.blockRepository.countBlocks();
   }
 }
